b86af1ee8a807d4f325042d7b9888a0fd88fea69
TEpolito <tepolito55@gmail.com>
2017-12-05 19:10:15 -0500

    removed config/auth and database from ignore

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://tepolito:qwerty@ds131826.mlab.com:31826/first-mlab-db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
